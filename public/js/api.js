62c3f8369c894b430716c5239ecf9d3e103d824d
Hritik Kumar <hritik.kmr.111@gmail.com>
2022-02-07 22:56:31 +0530

    done

---
+++ public/js/api.js
@@ -0,0 +1,10 @@
+let api_key = "a1eece75070003b0a2b60184dab6d0c8";
+
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+
+
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
